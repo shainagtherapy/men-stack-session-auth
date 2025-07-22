ff06851f8256cfe4ad86df7b9d55d6f16e4bddd4
Shaina G <shainagtherapy@gmail.com>
2025-07-22 10:28:12 -0700

    user sign in & i express-session

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const session = require('express-session')
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
+
+const authController = require("./controllers/auth.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -22,13 +25,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+app.use(session ({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+})
+)
 
-
+app.use("/auth", authController);
 
 // GET
 app.get("/", async (req, res) => {
     res.render('index.ejs')
 })
+
+
 
 
 
